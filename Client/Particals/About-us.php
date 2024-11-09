9bc0af9c416293b080ccd008b2f97aa4d699f28e
daBonk144 <nhatnthpc07949@fpt.edu.vn>
2024-11-09 18:15:15 +0700

    [Client[ - About-us & Blog & Product-All & Services

---
+++ Client/Particals/About-us.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+            <!--================Our Bakery Area =================-->
+            <section class="our_bakery_area p_100">
+        	<div class="container">
+        		<div class="our_bakery_text">
+        			<h2>Cách tiếp cận tiệm bánh của chúng tôi</h2>
+        			<h6>Nhưng để bạn có thể thấy tất cả sai lầm bẩm sinh này của những người buộc tội niềm vui và ca ngợi nỗi đau là từ đâu, tôi sẽ mở ra toàn bộ vấn đề và giải thích chính những điều đã được nói bởi người khám phá ra sự thật và cũng như người kiến ​​tạo nên sự thật. một cuộc sống hạnh phúc.</h6>
+        			<p>Vì không ai khinh thường, ghét bỏ hay chạy trốn khỏi thú vui vì đó là thú vui, mà vì những người không biết theo đuổi thú vui bằng lý trí sẽ phải chịu những đau khổ lớn lao. Hơn nữa, cũng không có ai vì thích đau đớn nên theo đuổi nó, muốn đạt được nó, nhưng vì những lúc như vậy không bao giờ xảy ra mà người đó tìm kiếm niềm vui lớn lao nào đó thông qua lao động và đau đớn. Đến từng chi tiết nhỏ nhất, bất kỳ ai trong chúng ta cũng tập thể dục.</p>
+        		</div>
+        		<div class="row our_bakery_image">
+        			<div class="col-md-4 col-6">
+        				<img class="img-fluid" src="img/our-bakery/bakery-1.jpg" alt="">
+        			</div>
+        			<div class="col-md-4 col-6">
+        				<img class="img-fluid" src="img/our-bakery/bakery-2.jpg" alt="">
+        			</div>
+        			<div class="col-md-4 col-6">
+        				<img class="img-fluid" src="img/our-bakery/bakery-3.jpg" alt="">
+        			</div>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Our Bakery Area =================-->
+        <!--================Our Mission Area =================-->
+        <section class="our_mission_area p_100">
+        	<div class="container">
+        		<div class="row our_mission_inner">
+        			<div class="col-lg-3">
+        				<div class="single_m_title">
+        					<h2>Sứ mệnh của chúng tôi</h2>
+        				</div>
+        			</div>
+        			<div class="col-lg-9">
+        				<div class="mission_inner_text">
+        					<h6>Nhưng để bạn có thể thấy tất cả những sai lầm bẩm sinh này là niềm vui của những người buộc tội và nỗi đau của những người khen ngợi, tôi sẽ mở đầu toàn bộ vấn đề và giải thích chính những điều mà người phát hiện ra sự thật đó đã nói và, như đã nói. , kiến ​​trúc sư của một cuộc sống hạnh phúc.</h6>
+        					<p>Vì không ai khinh thường, ghét bỏ hay chạy trốn khỏi thú vui vì đó là thú vui, mà vì những người không biết theo đuổi thú vui bằng lý trí sẽ phải chịu những đau khổ lớn lao. Hơn nữa, cũng không có ai vì thích đau đớn nên theo đuổi nó, muốn đạt được nó, nhưng vì những lúc như vậy không bao giờ xảy ra mà người đó tìm kiếm niềm vui lớn lao nào đó thông qua lao động và đau đớn. Để đi đến chi tiết nhỏ nhất, ai trong chúng ta thực hiện bất kỳ bài tập thể chất nào tốn nhiều công sức, ngoại trừ để thu được lợi ích nào đó từ nó? Nhưng ai có thể trách cứ người muốn hưởng thụ niềm vui mà không gây khó chịu, hay người trốn tránh nỗi đau không sánh bằng niềm vui?</p>
+        					<ul class="nav">
+        						<li><a href="#">Bánh tùy chỉnh</a></li>
+        						<li><a href="#">Bánh sinh nhật</a></li>
+        						<li><a href="#">Bánh cưới</a></li>
+        						<li><a href="#">Món ngon Châu Âu</a></li>
+        					</ul>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Our Mission Area =================-->
+        <!--================Client Says Area =================-->
+        <section class="client_says_area p_100">
+        	<div class="container">
+        		<div class="client_says_inner">
+        			<div class="c_says_title">
+        				<h2>Khách hàng của chúng tôi nói gì</h2>
+        			</div>
+        			<div class="client_says_slider owl-carousel">
+        				<div class="item">
+        					<div class="media">
+        						<div class="d-flex">
+        							<img src="img/client/client-1.png" alt="">
+        							<h3>“</h3>
+        						</div>
+        						<div class="media-body">
+        							<p>Bánh ngon nhưng đầu bếp hơi nóng tính</p>
+        							<h5>- Minh Á Quân</h5>
+        						</div>
+        					</div>
+        				</div>
+        				<div class="item">
+        					<div class="media">
+        						<div class="d-flex">
+        							<img src="img/client/client-1.png" alt="">
+        						</div>
+        						<div class="media-body">
+        							<p>Bánh tuyệt đỉnh nóc kịch trần siêu cấp vip pro</p>
+        							<h5>- Dương Gió Tai</h5>
+        						</div>
+        					</div>
+        				</div>
+        				<div class="item">
+        					<div class="media">
+        						<div class="d-flex">
+        							<img src="img/client/client-1.png" alt="">
+        						</div>
+        						<div class="media-body">
+        							<p>Bánh ăn vào thay đổi nhận thức nhân sinh quan về cuộc sống giúp tôi sống một cuộc đời mới</p>
+        							<h5>- Àpple Strong German</h5>
+        						</div>
+        					</div>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Client Says Area =================-->
+        <!--================End Client Says Area =================-->
+        <section class="our_chef_area p_100">
+        	<div class="container">
+        		<div class="row our_chef_inner">
+        			<div class="col-lg-3">
+        				<div class="chef_text_item">
+        					<div class="main_title">
+								<h2>Our Chefs</h2>
+								<p>Lorem ipsum dolor sit amet, cons ectetur elit. Vestibulum nec odios Suspe ndisse cursus mal suada faci lisis. Lorem ipsum dolor sit ametion.</p>
+							</div>
+        				</div>
+        			</div>
+        			<div class="col-lg-3 col-6">
+        				<div class="chef_item">
+        					<div class="chef_img">
+        						<img class="img-fluid" src="img/chef/chef-1.jpg" alt="">
+        					</div>
+        					<a href="#"><h4>Michale Joe</h4></a>
+        					<h5>Expert in Cake Making</h5>
+        				</div>
+        			</div>
+        			<div class="col-lg-3 col-6">
+        				<div class="chef_item">
+        					<div class="chef_img">
+        						<img class="img-fluid" src="img/chef/chef-2.jpg" alt="">
+        					</div>
+        					<a href="#"><h4>Michale Joe</h4></a>
+        					<h5>Expert in Cake Making</h5>
+        				</div>
+        			</div>
+        			<div class="col-lg-3 col-6">
+        				<div class="chef_item">
+        					<div class="chef_img">
+        						<img class="img-fluid" src="img/chef/chef-3.jpg" alt="">
+        					</div>
+        					<a href="#"><h4>Michale Joe</h4></a>
+        					<h5>Expert in Cake Making</h5>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Client Says Area =================-->
+</body>
+</html>
---
+++ Client/Particals/Blog.php
@@ -0,0 +1,210 @@
+        <!--================Blog Main Area =================-->
+        <section class="main_blog_area p_100">
+        	<div class="container">
+        		<div class="row blog_area_inner">
+        			<div class="col-lg-9">
+        				<div class="main_blog_inner">
+							<div class="blog_item">
+								<div class="blog_img">
+									<img class="img-fluid" src="./Client/assets/img/blog/main-blog/blog-1.jpg" alt="">
+								</div>
+								<div class="blog_text">
+									<div class="blog_time">
+										<div class="float-left">
+											<a href="#">08 Feb. 2018</a>
+										</div>
+										<div class="float-right">
+											<ul class="list_style">
+												<li><a href="#">By :  Admin</a></li>
+												<li><a href="#">bekery, sweet</a></li>
+												<li><a href="#">Comments: 8</a></li>
+											</ul>
+										</div>
+									</div>
+									<a href="#"><h4>This mistaken idea of denouncing pleasure and praising pain</h4></a>
+									<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni do-lores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
+									<a class="pink_more" href="#">Read more</a>
+								</div>
+							</div>
+							<div class="blog_item">
+								<div class="blog_img">
+									<img class="img-fluid" src="./Client/assets/img/blog/main-blog/blog-2.jpg" alt="">
+								</div>
+								<div class="blog_text">
+									<div class="blog_time">
+										<div class="float-left">
+											<a href="#">08 Feb. 2018</a>
+										</div>
+										<div class="float-right">
+											<ul class="list_style">
+												<li><a href="#">By :  Admin</a></li>
+												<li><a href="#">bekery, sweet</a></li>
+												<li><a href="#">Comments: 8</a></li>
+											</ul>
+										</div>
+									</div>
+									<a href="#"><h4>This mistaken idea of denouncing pleasure and praising pain</h4></a>
+									<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni do-lores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
+									<a class="pink_more" href="#">Read more</a>
+								</div>
+							</div>
+							<div class="blog_item">
+								<div class="blog_img">
+									<img class="img-fluid" src="./Client/assets/img/blog/main-blog/blog-3.jpg" alt="">
+								</div>
+								<div class="blog_text">
+									<div class="blog_time">
+										<div class="float-left">
+											<a href="#">08 Feb. 2018</a>
+										</div>
+										<div class="float-right">
+											<ul class="list_style">
+												<li><a href="#">By :  Admin</a></li>
+												<li><a href="#">bekery, sweet</a></li>
+												<li><a href="#">Comments: 8</a></li>
+											</ul>
+										</div>
+									</div>
+									<a href="#"><h4>This mistaken idea of denouncing pleasure and praising pain</h4></a>
+									<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni do-lores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
+									<a class="pink_more" href="#">Read more</a>
+								</div>
+							</div>
+							<div class="blog_item">
+								<div class="blog_img">
+									<img class="img-fluid" src="./Client/assets/img/blog/main-blog/blog-4.jpg" alt="">
+								</div>
+								<div class="blog_text">
+									<div class="blog_time">
+										<div class="float-left">
+											<a href="#">08 Feb. 2018</a>
+										</div>
+										<div class="float-right">
+											<ul class="list_style">
+												<li><a href="#">By :  Admin</a></li>
+												<li><a href="#">bekery, sweet</a></li>
+												<li><a href="#">Comments: 8</a></li>
+											</ul>
+										</div>
+									</div>
+									<a href="#"><h4>This mistaken idea of denouncing pleasure and praising pain</h4></a>
+									<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni do-lores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
+									<a class="pink_more" href="#">Read more</a>
+								</div>
+							</div>
+							<div class="blog_item">
+								<div class="blog_img">
+									<img class="img-fluid" src="./Client/assets/img/blog/main-blog/blog-5.jpg" alt="">
+								</div>
+								<div class="blog_text">
+									<div class="blog_time">
+										<div class="float-left">
+											<a href="#">08 Feb. 2018</a>
+										</div>
+										<div class="float-right">
+											<ul class="list_style">
+												<li><a href="#">By :  Admin</a></li>
+												<li><a href="#">bekery, sweet</a></li>
+												<li><a href="#">Comments: 8</a></li>
+											</ul>
+										</div>
+									</div>
+									<a href="#"><h4>This mistaken idea of denouncing pleasure and praising pain</h4></a>
+									<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni do-lores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
+									<a class="pink_more" href="#">Read more</a>
+								</div>
+							</div>
+        				</div>
+						<nav aria-label="Page navigation example" class="blog_pagination">
+							<ul class="pagination">
+								<li class="page-item active"><a class="page-link" href="#">1</a></li>
+								<li class="page-item"><a class="page-link" href="#">2</a></li>
+								<li class="page-item"><a class="page-link" href="#">3</a></li>
+							</ul>
+						</nav>
+        			</div>
+        			<div class="col-lg-3">
+        				<div class="right_sidebar_area">
+        					<aside class="r_widget search_widget">
+								<div class="input-group">
+									<input type="text" class="form-control" placeholder="Enter Search Keywords">
+									<div class="input-group-append">
+										<button class="btn" type="button"><i class="icon icon-Search"></i></button>
+									</div>
+								</div>
+        					</aside>
+        					<aside class="r_widget categories_widget">
+								<div class="r_title">
+									<h3>Categories</h3>
+								</div>
+       							<ul class="list_style">
+       								<li><a href="#">Chocolates<i class="fa fa-angle-right" aria-hidden="true"></i></a></li>
+       								<li><a href="#">Summer Caket<i class="fa fa-angle-right" aria-hidden="true"></i></a></li>
+       								<li><a href="#">Ice Cream<i class="fa fa-angle-right" aria-hidden="true"></i></a></li>
+       								<li><a href="#">Lollipop<i class="fa fa-angle-right" aria-hidden="true"></i></a></li>
+       								<li><a href="#">Sweet<i class="fa fa-angle-right" aria-hidden="true"></i></a></li>
+       								<li><a href="#">Bakery<i class="fa fa-angle-right" aria-hidden="true"></i></a></li>
+       							</ul>
+        					</aside>
+        					<aside class="r_widget recent_widget">
+								<div class="r_title">
+									<h3>Recent News</h3>
+								</div>
+       							<div class="recent_w_inner">
+       								<div class="media">
+										<div class="d-flex">
+											<img src="./Client/assets/img/blog/recent-post/r-post-1.jpg" alt="">
+										</div>
+										<div class="media-body">
+											<a href="#"><h4>A very bad quack might jinx zippy fowls ...</h4></a>
+											<a href="#"><p>08 Feb 2018</p></a>
+										</div>
+									</div>
+									<div class="media">
+										<div class="d-flex">
+											<img src="./Client/assets/img/blog/recent-post/r-post-2.jpg" alt="">
+										</div>
+										<div class="media-body">
+											<a href="#"><h4>Big Steps to change management succes...</h4></a>
+											<a href="#"><p>08 Feb 2018</p></a>
+										</div>
+									</div>
+									<div class="media">
+										<div class="d-flex">
+											<img src="./Client/assets/img/blog/recent-post/r-post-3.jpg" alt="">
+										</div>
+										<div class="media-body">
+											<a href="#"><h4>A paradisematic country, where roasted parts...</h4></a>
+											<a href="#"><p>08 Feb 2018</p></a>
+										</div>
+									</div>
+									<div class="media">
+										<div class="d-flex">
+											<img src="./Client/assets/img/blog/recent-post/r-post-4.jpg" alt="">
+										</div>
+										<div class="media-body">
+											<a href="#"><h4>A wonderful serenity has taken possession...</h4></a>
+											<a href="#"><p>08 Feb 2018</p></a>
+										</div>
+									</div>
+       							</div>
+        					</aside>
+        					<aside class="r_widget tags_widget">
+								<div class="r_title">
+									<h3>Tags</h3>
+								</div>
+       							<ul class="list_style">
+       								<li><a href="#">Baking Tips</a></li>
+       								<li><a href="#">Bakery</a></li>
+       								<li><a href="#">Recipe</a></li>
+       								<li><a href="#">Baking Tool</a></li>
+       								<li><a href="#">Cakes</a></li>
+       								<li><a href="#">Chocolates</a></li>
+       							</ul>
+        					</aside>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Blog Main Area =================-->

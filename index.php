28eda657d11d9026dfa6af531f6e51d5457c20be
thanhncpc03458 <110118893+thanhncpc03458@users.noreply.github.com>
2024-11-09 17:38:45 +0700

    Update Index

--- index.php
+++ index.php
@@ -1,26 +1,73 @@
 <!DOCTYPE html>
 <html lang="en">
-
+    
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ONEMORE</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <link rel="icon" href="img/fav-icon.png" type="image/x-icon" />
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Cake - Bakery</title>
 
-</head>
+        <!-- Icon css link -->
+        <link href="./Client/assets/css/font-awesome.min.css" rel="stylesheet">
+        <link href="./Client/assets/vendors/linearicons/style.css" rel="stylesheet">
+        <link href="./Client/assets/vendors/flat-icon/flaticon.css" rel="stylesheet">
+        <!-- Bootstrap -->
+        <link href="./Client/assets/css/bootstrap.min.css" rel="stylesheet">
+        
+        <!-- Rev slider css -->
+        <link href="./Client/assets/vendors/revolution/css/settings.css" rel="stylesheet">
+        <link href="./Client/assets/vendors/revolution/css/layers.css" rel="stylesheet">
+        <link href="./Client/assets/vendors/revolution/css/navigation.css" rel="stylesheet">
+        <link href="./Client/assets/vendors/animate-css/animate.css" rel="stylesheet">
+        
+        <!-- Extra plugin css -->
+        <link href="./Client/assets/vendors/owl-carousel/owl.carousel.min.css" rel="stylesheet">
+        <link href="./Client/assets/vendors/magnifc-popup/magnific-popup.css" rel="stylesheet">
+        
+        <link href="./Client/assets/css/style.css" rel="stylesheet">
+        <link href="./Client/assets/css/responsive.css" rel="stylesheet">
 
-<body>
-    <div class="container">
-
-        <?php include 'Client/Particals/Header.php' ?>
-        <?php include 'Client/Particals/Slider.php' ?>
-        <?php include 'Client/Particals/Product.php' ?>
-        <?php include 'Client/Particals/Footer.php' ?>
-
-    </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz4fnFO9gybYB1F1gZ3Q4U4z1U4yU4yU4yU4yU4yU4yU4yU4yU4yU4yU4yU4yU4" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-9aYkV6yR+P4g5Q6e6C8hE8z6g5Q6e6C8hE8z6g5Q6e6C8hE8z6g5Q6e6C8hE8" crossorigin="anonymous"></script>
-</body>
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+    <?php include 'Client/Particals/Header.php' ?>
+    <?php include 'Client/Particals/Slider.php' ?>
+    <?php include 'Client/Particals/Product.php' ?>
+        
+    <?php include 'Client/Particals/Footer.php' ?>
+        
+        
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="./Client/assets/js/jquery-3.2.1.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="./Client/assets/js/popper.min.js"></script>
+        <script src="./Client/assets/js/bootstrap.min.js"></script>
+        <!-- Rev slider js -->
+        <script src="./Client/assets/vendors/revolution/js/jquery.themepunch.tools.min.js"></script>
+        <script src="./Client/assets/vendors/revolution/js/jquery.themepunch.revolution.min.js"></script>
+        <script src="./Client/assets/vendors/revolution/js/extensions/revolution.extension.video.min.js"></script>
+        <script src="./Client/assets/vendors/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+        <script src="./Client/assets/vendors/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+        <script src="./Client/assets/vendors/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+        <!-- Extra plugin js -->
+        <script src="./Client/assets/vendors/owl-carousel/owl.carousel.min.js"></script>
+        <script src="./Client/assets/vendors/magnifc-popup/jquery.magnific-popup.min.js"></script>
+        <script src="./Client/assets/vendors/datetime-picker/js/moment.min.js"></script>
+        <script src="./Client/assets/vendors/datetime-picker/js/bootstrap-datetimepicker.min.js"></script>
+        <script src="./Client/assets/vendors/nice-select/js/jquery.nice-select.min.js"></script>
+        <script src="./Client/assets/vendors/jquery-ui/jquery-ui.min.js"></script>
+        <script src="./Client/assets/vendors/lightbox/simpleLightbox.min.js"></script>
+        
+        <script src="./Client/assets/js/theme.js"></script>
+    </body>
 
 </html>
+hello

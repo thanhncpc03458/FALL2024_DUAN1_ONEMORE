b293058e3f297f4912687708df87749dfb26b140
thanhncpc03458 <110118893+thanhncpc03458@users.noreply.github.com>
2024-11-10 11:08:10 +0700

    Update Product & Header & Cart

--- Client/Particals/Header.php
+++ Client/Particals/Header.php
@@ -24,8 +24,8 @@
 				<div class="container">
 					<nav class="navbar navbar-expand-lg navbar-light bg-light">
 						<a class="navbar-brand" href="index.html">
-						<img src="img/logo.png" alt="">
-						<img src="img/logo-2.png" alt="">
+						<img src="Client/assets/img/logo.png" alt="">
+						<img src="Client/assets/img/logo-2.png" alt="">
 						</a>
 						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 							<span class="my_toggle_menu">

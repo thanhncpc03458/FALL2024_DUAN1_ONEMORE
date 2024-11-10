41e2e80d5cf0f28a005cbc9346d54de0a17bd251
MinhG1910 <150761794+MinhG1910@users.noreply.github.com>
2024-11-10 10:05:47 +0700

    [Client]] Product-details, Checkout, Contact, Our-Cakes, Cart

---
+++ Client/Particals/Our-Cakes.php
@@ -0,0 +1,227 @@
+        <!--================End Main Header Area =================-->
+        <section class="banner_area">
+        	<div class="container">
+        		<div class="banner_text">
+        			<h3>Our Cakes</h3>
+        			<ul>
+        				<li><a href="index.html">Home</a></li>
+        				<li><a href="cakes.html">Services</a></li>
+        			</ul>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Main Header Area =================-->
+        
+        <!--================Blog Main Area =================-->
+        <section class="our_cakes_area p_100">
+        	<div class="container">
+        		<div class="main_title">
+        			<h2>Our Cakes</h2>
+        			<h5>Nhưng để bạn có thể thấy tất cả sai lầm bẩm sinh này của những người buộc tội niềm vui và ca ngợi nỗi đau là từ đâu, tôi sẽ mở rộng toàn bộ vấn đề và giải thích chính những điều đã được nói bởi người khám phá ra sự thật và cũng như người kiến ​​tạo nên sự thật. một cuộc sống hạnh phúc.<h5>
+        		</div>
+        		<div class="cake_feature_row row">
+					<div class="col-lg-3 col-md-4 col-6">
+						<div class="cake_feature_item">
+							<div class="cake_img">
+								<img src="./Client/assets/img/cake-feature/c-feature-1.jpg" alt="">
+							</div>
+							<div class="cake_text">
+								<h4>$29</h4>
+								<h3>Bánh Flan</h3>
+								<a class="pest_btn" href="#">Thêm Vào Giỏ Hàng</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-4 col-6">
+						<div class="cake_feature_item">
+							<div class="cake_img">
+								<img src="./Client/assets/img/cake-feature/c-feature-2.jpg" alt="">
+							</div>
+							<div class="cake_text">
+								<h4>$29</h4>
+								<h3>Strawberry Cupcakes</h3>
+								<a class="pest_btn" href="#">Thêm Vào Giỏ Hàng</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-4 col-6">
+						<div class="cake_feature_item">
+							<div class="cake_img">
+								<img src="./Client/assets/img/cake-feature/c-feature-3.jpg" alt="">
+							</div>
+							<div class="cake_text">
+								<h4>$29</h4>
+								<h3>Strawberry Cupcakes</h3>
+								<a class="pest_btn" href="#">Thêm Vào Giỏ Hàng</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-4 col-6">
+						<div class="cake_feature_item">
+							<div class="cake_img">
+								<img src="./Client/assets/img/cake-feature/c-feature-9.jpg" alt="">
+							</div>
+							<div class="cake_text">
+								<h4>$29</h4>
+								<h3>Strawberry Cupcakes</h3>
+								<a class="pest_btn" href="#">Thêm Vào Giỏ Hàng</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-4 col-6">
+						<div class="cake_feature_item">
+							<div class="cake_img">
+								<img src="./Client/assets/img/cake-feature/c-feature-4.jpg" alt="">
+							</div>
+							<div class="cake_text">
+								<h4>$29</h4>
+								<h3>Strawberry Cupcakes</h3>
+								<a class="pest_btn" href="#">Thêm Vào Giỏ Hàng</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-4 col-6">
+						<div class="cake_feature_item">
+							<div class="cake_img">
+								<img src="./Client/assets/img/cake-feature/c-feature-5.jpg" alt="">
+							</div>
+							<div class="cake_text">
+								<h4>$29</h4>
+								<h3>Strawberry Cupcakes</h3>
+								<a class="pest_btn" href="#">Thêm Vào Giỏ Hàng</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-4 col-6">
+						<div class="cake_feature_item">
+							<div class="cake_img">
+								<img src="./Client/assets/img/cake-feature/c-feature-6.jpg" alt="">
+							</div>
+							<div class="cake_text">
+								<h4>$29</h4>
+								<h3>Strawberry Cupcakes</h3>
+								<a class="pest_btn" href="#">Thêm Vào Giỏ Hàng</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-4 col-6">
+						<div class="cake_feature_item">
+							<div class="cake_img">
+								<img src="./Client/assets/img/cake-feature/c-feature-7.jpg" alt="">
+							</div>
+							<div class="cake_text">
+								<h4>$29</h4>
+								<h3>Strawberry Cupcakes</h3>
+								<a class="pest_btn" href="#">Thêm Vào Giỏ Hàng</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-4 col-6">
+						<div class="cake_feature_item">
+							<div class="cake_img">
+								<img src="./Client/assets/img/cake-feature/c-feature-8.jpg" alt="">
+							</div>
+							<div class="cake_text">
+								<h4>$29</h4>
+								<h3>Strawberry Cupcakes</h3>
+								<a class="pest_btn" href="#">Thêm Vào Giỏ Hàng</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-4 col-6">
+						<div class="cake_feature_item">
+							<div class="cake_img">
+								<img src="./Client/assets/img/cake-feature/c-feature-9.jpg" alt="">
+							</div>
+							<div class="cake_text">
+								<h4>$29</h4>
+								<h3>Strawberry Cupcakes</h3>
+								<a class="pest_btn" href="#">Thêm Vào Giỏ Hàng</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-4 col-6">
+						<div class="cake_feature_item">
+							<div class="cake_img">
+								<img src="./Client/assets/img/cake-feature/c-feature-3.jpg" alt="">
+							</div>
+							<div class="cake_text">
+								<h4>$29</h4>
+								<h3>Strawberry Cupcakes</h3>
+								<a class="pest_btn" href="#">Thêm Vào Giỏ Hàng</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-4 col-6">
+						<div class="cake_feature_item">
+							<div class="cake_img">
+								<img src="./Client/assets/img/cake-feature/c-feature-5.jpg" alt="">
+							</div>
+							<div class="cake_text">
+								<h4>$29</h4>
+								<h3>Strawberry Cupcakes</h3>
+								<a class="pest_btn" href="#">Thêm Vào Giỏ Hàng</a>
+							</div>
+						</div>
+					</div>
+				</div>
+        	</div>
+        </section>
+        <!--================End Blog Main Area =================-->
+        
+        <!--================Special Recipe Area =================-->
+        <section class="special_recipe_area">
+        	<div class="container">
+        		<div class="special_recipe_slider owl-carousel">
+        			<div class="item">
+        				<div class="media">
+        					<div class="d-flex">
+        						<img src="./Client/assets/img/recipe/recipe-1.png" alt="">
+        					</div>
+        					<div class="media-body">
+        						<h4>Công thức đặc biệt</h4>
+        						<p>Để đi đến từng chi tiết nhỏ nhất, không ai trong chúng ta thực hiện bất kỳ bài tập thể chất nào tốn nhiều công sức, trừ khi nó mang lại lợi ích nào đó cho người đó.</p>
+        						<a class="w_view_btn" href="#">Xem chi tiết</a>
+        					</div>
+        				</div>
+        			</div>
+        			<div class="item">
+        				<div class="media">
+        					<div class="d-flex">
+        						<img src="./Client/assets/img/recipe/recipe-1.png" alt="">
+        					</div>
+        					<div class="media-body">
+        						<h4>Công thức đặc biệt</h4>
+        						<p>Để đi đến từng chi tiết nhỏ nhất, không ai trong chúng ta thực hiện bất kỳ bài tập thể chất nào tốn nhiều công sức, trừ khi nó mang lại lợi ích nào đó cho người đó.</p>
+        						<a class="w_view_btn" href="#">Xem chi tiết</a>
+        					</div>
+        				</div>
+        			</div>
+        			<div class="item">
+        				<div class="media">
+        					<div class="d-flex">
+        						<img src="./Client/assets/img/recipe/recipe-1.png" alt="">
+        					</div>
+        					<div class="media-body">
+        						<h4>Công thức đặc biệt</h4>
+        						<p>Để đi đến từng chi tiết nhỏ nhất, không ai trong chúng ta thực hiện bất kỳ bài tập thể chất nào tốn nhiều công sức, trừ khi nó mang lại lợi ích nào đó cho người đó.</p>
+        						<a class="w_view_btn" href="#">Xem chi tiết</a>
+        					</div>
+        				</div>
+        			</div>
+        			<div class="item">
+        				<div class="media">
+        					<div class="d-flex">./Client/assets/
+        						<img src="./Client/assets/img/recipe/recipe-1.png" alt="">
+        					</div>
+        					<div class="media-body">
+        						<h4>Công thức đặc biệt</h4>
+        						<p>Để đi đến từng chi tiết nhỏ nhất, không ai trong chúng ta thực hiện bất kỳ bài tập thể chất nào tốn nhiều công sức, trừ khi nó mang lại lợi ích nào đó cho người đó.</p>
+        						<a class="w_view_btn" href="#">Xem chi tiết</a>
+        					</div>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Special Recipe Area =================-->
---
+++ Client/Particals/Product-details.php
@@ -0,0 +1,120 @@
+        <!--================End Main Header Area =================-->
+        <section class="banner_area">
+        	<div class="container">
+        		<div class="banner_text">
+        			<h3>Product Details</h3>
+        			<ul>
+        				<li><a href="index.html">Home</a></li>
+        				<li><a href="product-details.html">Product Details</a></li>
+        			</ul>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Main Header Area =================-->
+        
+        <!--================Product Details Area =================-->
+        <section class="product_details_area p_100">
+        	<div class="container">
+        		<div class="row product_d_price">
+        			<div class="col-lg-6">
+        				<div class="product_img"><img class="img-fluid" src="./Client/assets/img/product/product-details-1.jpg" alt=""></div>
+        			</div>
+        			<div class="col-lg-6">
+        				<div class="product_details_text">
+        					<h4>Brown Cake</h4>
+        					<p>Được làm từ Socola nguyên chất, kết hợp với trứng và sữa tươi</p>
+        					<h5>Giá :<span>$24.5</span></h5>
+        					<div class="quantity_box">
+        						<label for="quantity">Số Lượng :</label>
+        						<input type="text" placeholder="1" id="quantity">
+        					</div>
+        					<a class="pink_more" href="#">Thêm Vào Giỏ Hàng</a>
+        				</div>
+        			</div>
+        		</div>
+        		<div class="product_tab_area">
+					<nav>
+						<div class="nav nav-tabs" id="nav-tab" role="tablist">
+							<a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Miên tả</a>
+							<a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Công Thức Làm Bánh</a>
+							<a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Đánh Giá</a>
+						</div>
+					</nav>
+					<div class="tab-content" id="nav-tabContent">
+						<div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+							<p>Nothing</p>
+							<p>Nothing</p>
+						</div>
+						<div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+							<p>Nothing</p>
+							<p>Nothing</p>
+						</div>
+						<div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+							<p>Nothing</p>
+							<p>Nothing</p>
+						</div>
+					</div>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Product Details Area =================-->
+        
+        <!--================Similar Product Area =================-->
+        <section class="similar_product_area p_100">
+        	<div class="container">
+        		<div class="main_title">
+        			<h2>Sản phẩm tương tự</h2>
+        		</div>
+        		<div class="row similar_product_inner">
+        			<div class="col-lg-3 col-md-4 col-6">
+        				<div class="cake_feature_item">
+							<div class="cake_img">
+								<img src="./Client/assets/img/cake-feature/c-feature-1.jpg" alt="">
+							</div>
+							<div class="cake_text">
+								<h4>$29</h4>
+								<h3>Bánh Dâu</h3>
+								<a class="pest_btn" href="#">Thêm Vào Giỏ Hàng</a>
+							</div>
+						</div>
+        			</div>
+        			<div class="col-lg-3 col-md-4 col-6">
+        				<div class="cake_feature_item">
+							<div class="cake_img">
+								<img src="./Client/assets/img/cake-feature/c-feature-2.jpg" alt="">
+							</div>
+							<div class="cake_text">
+								<h4>$29</h4>
+								<h3>Bánh Dâu</h3>
+								<a class="pest_btn" href="#">Thêm Vào Giỏ Hàng</a>
+							</div>
+						</div>
+        			</div>
+        			<div class="col-lg-3 col-md-4 col-6">
+        				<div class="cake_feature_item">
+							<div class="cake_img">
+								<img src="./Client/assets/img/cake-feature/c-feature-3.jpg" alt="">
+							</div>
+							<div class="cake_text">
+								<h4>$29</h4>
+								<h3>Bánh Dâu</h3>
+								<a class="pest_btn" href="#">Thêm Vào Giỏ Hàng</a>
+							</div>
+						</div>
+        			</div>
+        			<div class="col-lg-3 col-md-4 col-6">
+        				<div class="cake_feature_item">
+							<div class="cake_img">
+								<img src="./Client/assets/img/cake-feature/c-feature-4.jpg" alt="">
+							</div>
+							<div class="cake_text">
+								<h4>$29</h4>
+								<h3>Bánh Dâu</h3>
+								<a class="pest_btn" href="#">Thêm Vào Giỏ Hàng</a>
+							</div>
+						</div>
+        			</div>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Similar Product Area =================-->

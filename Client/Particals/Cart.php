03daa9733ec77c65b6a751a7c91c06ab3f20cd05
MinhG1910 <150761794+MinhG1910@users.noreply.github.com>
2024-11-10 10:05:47 +0700

    [Client]] Product-details, Checkout, Contact, Our-Cakes, Cart

---
+++ Client/Particals/Cart.php
@@ -0,0 +1,91 @@
+
+               <!--================End Main Header Area =================-->
+               <section class="banner_area">
+        	<div class="container">
+        		<div class="banner_text">
+        			<h3>Cart</h3>
+        			<ul>
+        				<li><a href="index.html">Home</a></li>
+        				<li><a href="cart.html">Giỏ Hàng</a></li>
+        			</ul>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Main Header Area =================-->
+        
+        <!--================Cart Table Area =================-->
+        <section class="cart_table_area p_100">
+        	<div class="container">
+				<div class="table-responsive">
+					<table class="table">
+						<thead>
+							<tr>
+								<th scope="col">Ảnh</th>
+								<th scope="col">Tên Sản Phẩm</th>
+								<th scope="col">Giá</th>
+								<th scope="col">Số Lượng</th>
+								<th scope="col">Tổng Giá</th>
+								<th scope="col"></th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>
+									<img src="./Client/assets/img/product/cart-img.jpg" alt="">
+								</td>
+								<td>Bánh Dâu</td>
+								<td>$25.00</td>
+								<td>
+									<select class="product_select">
+										<option data-display="1">1</option>
+										<option value="1">2</option>
+										<option value="2">3</option> 
+										<option value="4">4</option>
+										<option value="5">5</option>
+									</select>
+								</td>
+								<td>$25.00</td>
+								<td>X</td>
+							</tr>
+							<tr>
+								<td>
+									<form class="form-inline"> 
+										<div class="form-group"> 
+											<input type="text" class="form-control" placeholder="Coupon code">
+										</div>
+										<button type="submit" class="btn">Apply Coupon</button>
+									</form>
+								</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td>
+									<a class="pest_btn" href="#">Thanh Toán</a>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+       			<div class="row cart_total_inner">
+        			<div class="col-lg-7"></div>
+        			<div class="col-lg-5">
+        				<div class="cart_total_text">
+        					<div class="cart_head">
+        						Giỏ Hàng
+        					</div>
+        					<div class="sub_total">
+        						<h5>Tổng Phụ <span>$25.00</span></h5>
+        					</div>
+        					<div class="total">
+        						<h4>Tổng Giá <span>$25.00</span></h4>
+        					</div>
+        					<div class="cart_footer">
+        						<a class="pest_btn" href="#">Tiến hành thanh toán</a>
+        					</div>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Cart Table Area =================-->
